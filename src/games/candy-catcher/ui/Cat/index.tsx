788a02a0d0ec9c4a11ab5593a24ba25fe791d5e4
Sergey Alexeev <sergfry@gmail.com>
2023-07-01 16:49:38 +0200

    Handle pointer move

--- src/games/candy-catcher/ui/Cat/index.tsx
+++ src/games/candy-catcher/ui/Cat/index.tsx
@@ -2,15 +2,16 @@ import { Sprite } from "@pixi/react";
 
 type CatProps = {
   image: string
+  x: number
 }
 
-export const Cat = ({ image }: CatProps) => {
+export const Cat = ({ image, x }: CatProps) => {
   return (
     <Sprite
       image={image}
       scale={{ x: 0.7, y: 0.7 }}
       anchor={0.5}
-      x={100}
+      x={x}
       y={400}
     />
   )
--- src/games/candy-catcher/ui/Stage/index.tsx
+++ src/games/candy-catcher/ui/Stage/index.tsx
@@ -1,17 +1,27 @@
 import { Container, Stage as PixiStage, Text } from "@pixi/react";
 import { Candy } from "../Candy";
 import { Cat } from "../Cat";
+import { useCallback, useState } from "react";
 
 export const Stage = () => {
+  const [catX, setCatX] = useState(0)
+  const handlePointerMove = useCallback((e: React.PointerEvent<HTMLCanvasElement>) => {
+    setCatX(e.clientX)
+  }, [])
+
   return (
-    <PixiStage options={{ backgroundColor: '0xeef1f5' }} width={document.documentElement.clientWidth}>
+    <PixiStage
+      options={{ backgroundColor: 0xeef1f5 }}
+      width={document.documentElement.clientWidth}
+      onPointerMove={handlePointerMove}
+    >
       <Container x={400} y={330}>
         <Text text="Let's play it!" anchor={{ x: 0.5, y: 0.5 }} />
       </Container>
       <Candy image="assets/candy-catcher/index.png" x={100} />
       <Candy image="assets/candy-catcher/candies/1.png" x={400} />
       <Candy image="assets/candy-catcher/candies/2.png" x={600} />
-      <Cat image="assets/candy-catcher/catchers/cat.png" />
+      <Cat image="assets/candy-catcher/catchers/cat.png" x={catX} />
     </PixiStage>
   );
 };
