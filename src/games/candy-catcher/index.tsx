7795aabb63a84c76a20ddda9b41d1b95b5849a95
Sergey Alexeev <sergfry@gmail.com>
2023-07-01 16:34:49 +0200

    Remove home link

--- src/games/candy-catcher/index.tsx
+++ src/games/candy-catcher/index.tsx
@@ -3,7 +3,6 @@ import { Stage } from "./ui/Stage";
 export const CandyCatcher = () => {
   return (
     <>
-      <a href="/" title="Home">Home</a>
       <h1>Candy Catcher</h1>
       <Stage />
     </>
