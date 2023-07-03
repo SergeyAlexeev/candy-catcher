88b59ef532fd3ce75bb21cca6997dfc8b50d71c3
Sergey Alexeev <sergfry@gmail.com>
2023-07-03 13:28:17 +0200

    Update buttons

--- src/games/candy-catcher/ui/LeftButton/index.tsx
+++ src/games/candy-catcher/ui/LeftButton/index.tsx
@@ -1,4 +1,5 @@
 import { Sprite, useApp } from "@pixi/react";
+import { useState } from "react";
 
 type LeftButtonProps = {
   image: string;
@@ -9,19 +10,35 @@ type LeftButtonProps = {
 const SCALE = 0.5;
 const BUTTON_HEIGHT = 217 * SCALE;
 const SCREEN_OFFSET = 20;
+const PRESS_SHIFT = 5;
 
-export const LeftButton = ({ image, onPointerDown, onPointerUp }: LeftButtonProps) => {
-  const app = useApp()
+export const LeftButton = ({
+  image,
+  onPointerDown,
+  onPointerUp,
+}: LeftButtonProps) => {
+  const app = useApp();
+  const [y, setY] = useState(app.screen.height - BUTTON_HEIGHT - SCREEN_OFFSET);
+
+  const handlePointerDown = () => {
+    setY((prev) => prev + PRESS_SHIFT);
+    onPointerDown();
+  };
+
+  const handlePointerUp = () => {
+    setY((prev) => prev - PRESS_SHIFT);
+    onPointerUp();
+  };
 
   return (
     <Sprite
       image={image}
       scale={{ x: SCALE, y: SCALE }}
       x={SCREEN_OFFSET}
-      y={app.screen.height - BUTTON_HEIGHT - SCREEN_OFFSET}
+      y={y}
       interactive
-      pointerdown={onPointerDown}
-      pointerup={onPointerUp}
+      pointerdown={handlePointerDown}
+      pointerup={handlePointerUp}
     />
   );
 };
--- src/games/candy-catcher/ui/RightButton/index.tsx
+++ src/games/candy-catcher/ui/RightButton/index.tsx
@@ -1,4 +1,5 @@
 import { Sprite, useApp } from "@pixi/react";
+import { useState } from "react";
 
 type RightButtonProps = {
   image: string;
@@ -10,19 +11,36 @@ const SCALE = 0.5;
 const BUTTON_HEIGHT = 187 * SCALE;
 const BUTTON_WIDTH = 200 * SCALE;
 const SCREEN_OFFSET = 20;
+const PRESS_SHIFT = 5;
 
-export const RightButton = ({ image, onPointerDown, onPointerUp }: RightButtonProps) => {
-  const app = useApp()
+export const RightButton = ({
+  image,
+  onPointerDown,
+  onPointerUp,
+}: RightButtonProps) => {
+  const app = useApp();
+
+  const [y, setY] = useState(app.screen.height - BUTTON_HEIGHT - SCREEN_OFFSET);
+
+  const handlePointerDown = () => {
+    setY((prev) => prev + PRESS_SHIFT);
+    onPointerDown();
+  };
+
+  const handlePointerUp = () => {
+    setY((prev) => prev - PRESS_SHIFT);
+    onPointerUp();
+  };
 
   return (
     <Sprite
       image={image}
       scale={{ x: SCALE, y: SCALE }}
       x={app.screen.width - BUTTON_WIDTH - SCREEN_OFFSET}
-      y={app.screen.height - BUTTON_HEIGHT - SCREEN_OFFSET}
+      y={y}
       interactive
-      pointerdown={onPointerDown}
-      pointerup={onPointerUp}
+      pointerdown={handlePointerDown}
+      pointerup={handlePointerUp}
     />
   );
 };
